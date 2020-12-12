12a5ee0fc4f1e4f70c3ccd5c6097207a1c4a270e
on3dd <onedeadwave@gmail.com>
2020-12-12 22:28:54 +1000

    upd

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
+
+import GlobalStyle from './utils/globalStyle';
+
+import Todo from './components/Todo';
+
+const AppContainer = styled.div`
+  display: flex;
+  min-width: 100vw;
+  min-height: 100vh;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppContainer className="app">
+      <GlobalStyle />
+      <Todo />
+    </AppContainer>
   );
 }
 
---
+++ src/components/Todo.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const TodoContainer = styled.section`
+  margin: auto;
+`;
+
+const Todo: React.FC = () => {
+  return <TodoContainer>nigger</TodoContainer>;
+};
+
+export default Todo;
